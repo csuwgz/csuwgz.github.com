345e111a5a5738af30497872188da373bf262021
Guizhong Wang <wgzwingwing@gmail.com>
2014-06-11 21:38:48 +0800

    1.01
    CommitTest

---
+++ MyFirstSwiftCommandLine/MyFirstSwiftCommandLine/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  MyFirstSwiftCommandLine
+//
+//  Created by apple on 14-6-7.
+//  Copyright (c) 2014年 csu. All rights reserved.
+//
+
+import Foundation
+/*hello world*/
+//println("Hello, World!")
+
+/*变量*/
+//var myValue=6
+
+/*常量*/
+//let explictFloat :Float = 4
+//let explictDouble :Double = 3
+//println("the value of the explictFloat is \(explictFloat)")
+
+/*类型转化*/
+//var intValue = 5
+//var strValue = "My age is:"
+//var strAge = strValue + String(intValue)
+//println(strAge)
+
+/**/
+
+
+
+
+
